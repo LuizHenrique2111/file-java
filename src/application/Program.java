51b4fd922eab0652882b73d0a3652473ddcc3e71
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-06-20 18:57:42 -0300

    FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,15 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		File file = new File("C:\\Games\\in.txt");
+		// File
+		/*File file = new File("C:\\Games\\in.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
@@ -20,6 +21,36 @@ public class Program {
 		} finally {
 			if (sc != null) {
 				sc.close();
+			}
+		}*/
+		
+		// ------------------------------------------------------------------
+		
+		String path = "C:\\Games\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		} finally {
+			try {				
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
